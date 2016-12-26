8fc99b11f5d9bef862d10a32fba10894cb8eb5e9
Ssssory <itrubchaninov@list.ru>
2016-12-26 17:55:05 +0300

    correct array to two-dimensional array. redrow method map.

--- index.php
+++ index.php
@@ -15,29 +15,34 @@ require_once "metods.php";
 
 
   <?php
-  $start_arr = [1,5,5,10,2,15,5,10,5,2];
-  $line2_arr = [15,5,5,9,5,12,15,9,5,2];
-  $line3_arr = [13,5,2,15,5,10,9,5,5,14];
-  $line4_arr = [0,0,0,0,0,0,0,0,0,0];
-  $line5_arr = [0,0,0,0,0,0,0,0,0,0];
-  $line6_arr = [0,0,0,0,0,0,0,0,0,0];
-  $line7_arr = [0,0,0,0,0,0,0,0,0,0];
-  $line8_arr = [0,0,0,0,0,0,0,0,0,0];
-  $line9_arr = [0,0,0,0,0,0,0,0,0,0];
-  $last_arr = [0,0,0,0,0,0,0,0,0,0];
+// создание карты. доработать на генераторе.
+  $start_arr = [1,5,5,10,2];
+  $line2_arr = [15,5,5,9,2];
+  $line3_arr = [13,10,2,15,2];
+  $line4_arr = [1,9,10,9,2];
+  $last_arr = [1,5,9,5,2];
+// $all_map исходная карта уровня.
+  $all_map = array(array('') );
+$all_map = add_arr_to_all_map($start_arr, $line2_arr, $line3_arr, $line4_arr, $last_arr);
+// карта для персонажа
+$person_y = 0;
+$person_x = 0;
+$all_map_drow = array(array('') );
+$all_map_drow = $all_map;
+$temp = $all_map_drow[$person_y][$person_x];
+$all_map_drow[$person_y][$person_x] = array("$temp","person");
 
-  $row_start = drow_row($start_arr);
+// выводим карту
+$drow_map = drow_row($all_map_drow);
+echo $drow_map;
+  /*$row_start = drow_row($start_arr);
   $row2 = drow_row($line2_arr);
   $row3 = drow_row($line3_arr);
   $row4 = drow_row($line4_arr);
   $row5 = drow_row($line5_arr);
-  $row6 = drow_row($line6_arr);
-  $row7 = drow_row($line7_arr);
-  $row8 = drow_row($line8_arr);
-  $row9 = drow_row($line9_arr);
-  $row_last = drow_row($last_arr);
 
-  echo  $row_start.$row2.$row3.$row4.$row5.$row6.$row7.$row8.$row9.$row_last;
+
+  echo  $row_start.$row2.$row3.$row4.$row5;*/
   ?>
   </div>
 
--- metods.php
+++ metods.php
@@ -20,66 +20,98 @@ function drow_nav(){
 }
 
 function sell_type_inner($n){
-  switch($n){
-    case 0:
-    return "smoke";
-    break;
-    case 1:
-    return "square wall_t wall_l wall_b";
-    break;
-    case 2:
-    return "square wall_t wall_r wall_b";
-    break;
-    case 3:
-    return "square wall_l wall_r wall_b";
-    break;
-    case 4:
-    return "square wall_t wall_l wall_r";
-    break;
-    case 5:
-    return "square wall_t wall_b";
-    break;
-    case 6:
-    return "square wall_l wall_r";
-    break;
-    case 7:
-    return "square wall_l";
-    break;
-    case 8:
-    return "square wall_r";
-    break;
-    case 9:
-    return "square wall_b";
-    break;
-    case 10:
-    return "square wall_t";
-    break;
-    case 11:
-    return "square";
-    break;
-    case 12:
-    return "square wall_r wall_b";
-    break;
-    case 13:
-    return "square wall_l wall_b";
-    break;
-    case 14:
-    return "square wall_t wall_r";
-    break;
-    case 15:
-    return "square wall_t wall_l";
-    break;
+  if (is_array($n)){
+    return sell_type_inner($n[0]);
+  }else{
+    switch($n){
+      case 0:
+      return "smoke";
+      break;
+      case 1:
+      return "square wall_t wall_l wall_b";
+      break;
+      case 2:
+      return "square wall_t wall_r wall_b";
+      break;
+      case 3:
+      return "square wall_l wall_r wall_b";
+      break;
+      case 4:
+      return "square wall_t wall_l wall_r";
+      break;
+      case 5:
+      return "square wall_t wall_b";
+      break;
+      case 6:
+      return "square wall_l wall_r";
+      break;
+      case 7:
+      return "square wall_l";
+      break;
+      case 8:
+      return "square wall_r";
+      break;
+      case 9:
+      return "square wall_b";
+      break;
+      case 10:
+      return "square wall_t";
+      break;
+      case 11:
+      return "square";
+      break;
+      case 12:
+      return "square wall_r wall_b";
+      break;
+      case 13:
+      return "square wall_l wall_b";
+      break;
+      case 14:
+      return "square wall_t wall_r";
+      break;
+      case 15:
+      return "square wall_t wall_l";
+      break;
+    }
   }
 }
 function drow_row($arr){
   $row='';
-  $base_arr= $arr;
-
-  for($i=0;$i<=9;$i++){
-    $row .= complete_sel(sell_type_inner($base_arr[$i]));
+  for($i=0;$i<5;$i++){
+    for($y=0;$y<5;$y++){
+      $row .= complete_sel(sell_type_inner($arr[$i][$y]));
+    }
   }
   return $row;
 }
+/*function drow_row($arr){
+  $row='';
+  $base_arr= $arr;
 
+  for($i=0;$i<5;$i++){
+    $row .= complete_sel(sell_type_inner($base_arr[$i]));
+  }
+  return $row;
+}*/
+
+function add_arr_to_all_map($arr1, $arr2, $arr3, $arr4, $arr5){
+  $ret_map = array(array('') );
+  for ($i=0; $i < count($arr1); $i++) {
+    $ret_map[0][$i] = $arr1[$i];
+  }
+  for ($i=0; $i < count($arr2); $i++) {
+    $ret_map[1][$i] = $arr2[$i];
+  }
+  for ($i=0; $i < count($arr3); $i++) {
+    $ret_map[2][$i] = $arr3[$i];
+  }
+  for ($i=0; $i < count($arr4); $i++) {
+    $ret_map[3][$i] = $arr4[$i];
+  }
+  for ($i=0; $i < count($arr5); $i++) {
+    $ret_map[4][$i] = $arr5[$i];
+  }
+return($ret_map);
+}
 
  ?>
